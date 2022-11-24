0ba9253bfd3b10abd88feea049afe25522c51085
Sajia09 <sajiaa09@gmail.com>
2022-11-25 04:35:57 +0600

    something added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
@@ -11,6 +11,29 @@ const app = express();
 // middleware
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hmhhmh5.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+async function run(){
+    try{
+
+        const categoryCollection = client.db('readerspick').collection('category');
+
+
+        app.get('/category', async (req, res) => {
+            const query = {};
+            const categories = await categoryCollection.find(query).toArray();
+            res.send(categories);
+        })
+    }
+    finally{
+
+    }
+}
+run().catch(console.log)
 
 app.get('/', async (req, res) => {
     res.send('Readers pick server is running');
