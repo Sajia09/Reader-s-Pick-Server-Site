40b0c8d4db3114de702c30e379a6a9f7af2b06fe
Sajia09 <sajiaa09@gmail.com>
2022-11-28 21:12:27 +0600

    users api added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
@@ -21,6 +21,8 @@ async function run(){
     try{
 
         const categoryCollection = client.db('readerspick').collection('category');
+        const booksCollection = client.db('readerspick').collection('books');
+        const usersCollection = client.db('readerspick').collection('users');
 
 
         app.get('/category', async (req, res) => {
@@ -28,6 +30,30 @@ async function run(){
             const categories = await categoryCollection.find(query).toArray();
             res.send(categories);
         })
+        app.get('/allbooks', async (req, res) => {
+            const query = {};
+            const allBooks = await booksCollection.find(query).toArray();
+            res.send(allBooks);
+        })
+        app.get('/category/:name', async (req, res) => {
+            const pname = req.params.name;
+            const filter = {category:pname };
+            const result = await booksCollection.find(filter).toArray();
+            res.send(result);
+        })
+
+        app.get('/users', async (req, res) => {
+            const query = {};
+            const users = await usersCollection.find(query).toArray();
+            res.send(users);
+        });
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            console.log(user);
+            const result = await usersCollection.insertOne(user);
+            res.send(result);
+        });
     }
     finally{
 
