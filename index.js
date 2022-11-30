0a8fb357607e6b09808d561ed49659f7d175c7d0
Sajia09 <sajiaa09@gmail.com>
2022-11-30 23:09:26 +0600

    final touch

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
-const port = process.env.PORT 
+const port = process.env.PORT || 5000;
 
 const app = express();
 
-// middleware
+
 app.use(cors());
 app.use(express.json());
 
